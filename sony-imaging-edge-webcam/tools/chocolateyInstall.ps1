d7f929f9da1c71a5d7b86a48cfcbdc3c58d25d48
stumbaumr <stumbaumr@yahoo.de>
2022-02-04 18:16:14 +0100

    Ad sha256 file hash

--- sony-imaging-edge-webcam/tools/chocolateyInstall.ps1
+++ sony-imaging-edge-webcam/tools/chocolateyInstall.ps1
@@ -1,9 +1,17 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 $packageName  = 'sony-imaging-edge-webcam'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$fileLocation = Get-ChocolateyWebFile -PackageName 'SonyIEW.exe' -FileFullPath "$toolsDir\SonyIEW.exe" -Url 'https://di.update.sony.net/NEX/ewH5TVuMLU/IEW111_2108a.exe'
+$webFileArgs = @{
+  packageName   = $packageName
+  FileFullPath  = '$toolsDir\SonyIEW.exe'
+  Url           = 'https://di.update.sony.net/NEX/ewH5TVuMLU/IEW111_2108a.exe'  
+  Checksum      = '2026AF500609738B19ED5B9B73FE72A995B7478133CACC4E4E15537D83D91327'
+  ChecksumType  = 'sha256'
+}
+
+$fileLocation = Get-ChocolateyWebFile @webFileArgs
 
 # Get publisher certificate from the file and add it to TrustedPublisher
 $cert = (Get-AuthenticodeSignature $fileLocation ).SignerCertificate;
